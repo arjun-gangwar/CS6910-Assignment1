232fa1cce4b6661f49f0e53aa0a0dcf4bb313248
Arjun Gangwar <arjungangwar@gmail.com>
2024-03-22 21:49:42 +0530

    clean up

--- assignment1/loss.py
+++ assignment1/loss.py
@@ -15,3 +15,19 @@ class CrossEntropyLoss:
         return []
     def d_parameters(self):
         return []
+    
+class MeanSquareLoss:
+    def __init__(self, epsilon):
+        self.epsilon = epsilon
+    def __call__(self, y_enc, y_hat):       # (B,), (B, classes)
+        self.y_enc = y_enc
+        self.y = np.argmax(y_enc, axis=-1)
+        self.y_hat = y_hat
+        self.out = np.mean(np.square(self.y_enc - self.y_hat)) / 2
+        return self.out
+    def diff(self):
+        return np.mean(-1 * (self.y_enc - self.y_hat), axis=0)
+    def parameters(self):
+        return []
+    def d_parameters(self):
+        return []
